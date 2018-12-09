6bdf7d932ae9ab3621c4154449c681f65e651c23
DefiantDog66 <wh0298i@yahoo.com>
2018-12-09 12:20:14 +0000

    Added messages
    Added sensible default messages to assertion functions.

--- index.js
+++ index.js
@@ -6,15 +6,15 @@ assert.ok = assert
 module.exports = assert
 
 function equal (a, b, m) {
-  assert(a == b, m) // eslint-disable-line eqeqeq
+  assert(a == b, m || 'Expected "' + a + '" to equal "' + b + '".') // eslint-disable-line eqeqeq
 }
 
 function notEqual (a, b, m) {
-  assert(a != b, m) // eslint-disable-line eqeqeq
+  assert(a != b, m || 'Expected "' + a + '" to not equal "' + b + '".') // eslint-disable-line eqeqeq
 }
 
 function notOk (t, m) {
-  assert(!t, m)
+  assert(!t, m || 'Expected "' + a + '" to be falsy.');
 }
 
 function assert (t, m) {
