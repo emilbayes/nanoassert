6423af659a35ba76fbcd311b9890e43d97fdaead
w0298i <wh0298i@yahoo.com>
2018-12-09 22:49:17 +0000

    Added fail, ifError, rejects, throws, isTrue and isFalse assertion functions.
    This closes the gap between this library and the native "assert"
    module for the most part. I've also added tests for these functions.

--- index.js
+++ index.js
@@ -2,8 +2,36 @@ assert.notEqual = notEqual
 assert.notOk = notOk
 assert.equal = equal
 assert.ok = assert
+assert.fail = fail
+assert.ifError = ifError
+assert.rejects = rejects
+assert.throws = throws
+assert.isTrue = isTrue
+assert.isFalse = isFalse
 
 module.exports = assert
+
+function isTrue(t, m) {
+  assert(t === true, m || 'Expected "' + t + '" to be true.')
+}
+
+function isFalse(t, m) {
+  assert(t === false, m || 'Expected "' + t + '" to be false.')
+}
+
+function _toPromise (v) {
+  if (v && typeof v.then === 'function') {
+    return v
+  } else if (v && typeof v === 'function') {
+    return v()
+  } else {
+    fail('Expected "asyncFn" to be a promise.');
+  }
+}
+
+function _isPromise (v) {
+  return v && typeof v.then === 'function'
+}
 
 function equal (a, b, m) {
   assert(a == b, m || 'Expected "' + a + '" to equal "' + b + '".') // eslint-disable-line eqeqeq
@@ -14,9 +42,38 @@ function notEqual (a, b, m) {
 }
 
 function notOk (t, m) {
-  assert(!t, m || 'Expected "' + a + '" to be falsy.');
+  assert(!t, m || 'Expected "' + t + '" to be falsy.')
 }
 
 function assert (t, m) {
-  if (!t) throw new Error(m || 'AssertionError')
+  if (!t) fail(m || '')
 }
+
+function fail (m) {
+  throw new Error(m || 'AssertionError')
+}
+
+function ifError (t) {
+  if (t != null) fail('ifError got unwanted exception: ' + t)
+}
+
+function rejects (asyncFn, err, m) {
+  // don't accept type `function | string | regexp` for now
+  err = err || Error
+
+  return new Promise(function (resolve, reject) {
+    // do the conversion here so bad functions reject the promise
+    asyncFn = _toPromise(asyncFn)
+    asyncFn.catch(resolve)
+  })
+}
+
+function throws(fn, err, m) {
+  err = err || Error
+  
+  try {
+    fn()
+    fail('Expected the function to throw an error.')
+  } catch (err) {}
+}
+
--- test.js
+++ test.js
@@ -5,15 +5,17 @@ var methods = {
   'notEqual': [true, false],
   'notOk': [false],
   'equal': [true, true],
-  'ok': [true]
+  'ok': [true],
+  'isTrue': [true],
+  'isFalse': [false]
 }
 
-test('test', function (t) {
+test('assert', function (t) {
   try {
     assert(true === true) // test that it doesn't throw
     t.pass('does not throw on truthy')
-  } catch (e) {
-    t.fail()
+  } catch (e) {!
+    t.fail('should not throw on truthy')
   }
 
   t.throws(assert.bind(null, false), 'throws on falsy')
@@ -34,11 +36,48 @@ test('test', function (t) {
     var args = methods[method]
     try {
       assert[method].apply(null, args)
-      t.pass('does not throw')
+      t.pass('passing "' + args.join(", ") + '" does not make "' + method + '" throw')
     } catch (e) {
-      t.fail()
+      t.fail('passing "' + args.join(', ') + '" should not make "' + method + '" throw');
     }
   })
 
   t.end()
 })
+
+test('fail', function (t) {
+  try {
+    assert.fail('hello');
+    t.fail('hello')
+  } catch (error) {
+    t.pass('throws an assertion error');
+  }
+
+  t.end();
+});
+
+test('ifError', function (t) {
+  t.plan();
+  try {
+    assert.ifError(2);
+    t.fail('it should throw if val isnt null | undefined');
+  } catch (error) {
+    t.pass('it did throw when passed a value');
+  }
+
+  try {
+    assert.ifError(undefined);
+    t.pass('it didnt throw when passed undefined');
+  } catch (error) {
+    t.fail('it shouldnt throw if val is undefined');
+  }
+
+  try {
+    assert.ifError(null);
+    t.pass('it didnt throw when passed null');
+  } catch (error) {
+    t.fail('it shouldnt throw if val is null');
+  }
+
+  t.end();
+});
