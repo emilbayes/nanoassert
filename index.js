a16233cc738a67b70e509a33f643beb9eddac7c2
Emil Bay <github@tixz.dk>
2019-01-21 13:33:27 +0100

    Add JSDoc

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ module.exports = assert
 class AssertionError extends Error {}
 AssertionError.prototype.name = 'AssertionError'
 
+/**
+ * Minimal assert function
+ * @param  {any} t Value to check if falsy
+ * @param  {string=} m Optional assertion error message
+ * @throws {AssertionError}
+ */
 function assert (t, m) {
   if (!t) {
     var err = new AssertionError(m)
