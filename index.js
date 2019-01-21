f49834c584bd07137196de4afdb62e719cac68f8
Emil Bay <github@tixz.dk>
2019-01-21 13:19:16 +0100

    Simplify stack logic and save bytes

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 module.exports = assert
 
-class AssertionError extends Error {
-  constructor (...args) {
-    super(...args)
-
-    if (Error.captureStackTrace) Error.captureStackTrace(this, assert)
-  }
-}
+class AssertionError extends Error {}
 AssertionError.prototype.name = 'AssertionError'
 
 function assert (t, m) {
-  if (!t) throw new AssertionError(m)
+  if (!t) {
+    var err = new AssertionError(m)
+    if (Error.captureStackTrace) Error.captureStackTrace(err, assert)
+    throw err
+  }
 }
